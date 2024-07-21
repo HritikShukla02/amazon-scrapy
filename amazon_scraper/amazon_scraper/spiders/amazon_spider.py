48f6453a84daf581fabad0ac34b2997c999ab0a9
HritikShukla02 <shritik83385@gmail.com>
2024-07-21 10:49:53 +0530

    initial commit

---
+++ amazon_scraper/amazon_scraper/spiders/amazon_spider.py
@@ -0,0 +1,49 @@
+from typing import Iterable
+import scrapy
+from amazon_scraper.items import AmazonScraperItem
+from urllib.parse import urlencode
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+def get_scrapeops_url(url):
+    payload = {'api_key': API_KEY, 'url': url}
+    proxy_url = 'https://proxy.scrapeops.io/v1/?' + urlencode(payload)
+    return proxy_url
+
+class AmazonSpiderSpider(scrapy.Spider):
+    name = "amazon_spider"
+    # allowed_domains = ["www.amazon.in"]
+    start_urls = ["https://www.amazon.in"]
+
+    def start_requests(self):
+        keyword = ['pendrive']
+
+        for word in keyword:
+            for page in range(10):
+                url = f'https://www.amazon.in/s?k={word}&page={page +1}'
+                yield scrapy.Request(get_scrapeops_url(url))
+
+    def parse(self, response):
+        
+        products = response.css('h2> a ::attr(href)').getall()
+        for prt in products:
+            prt_url = "https://www.amazon.in" + prt
+            yield response.follow(get_scrapeops_url(prt_url), callback=self.parse_prt)
+
+    def parse_prt(self, response):
+        object = AmazonScraperItem()
+        table = response.css('table.a-normal.a-spacing-micro tr')
+        object['name'] = response.css('span#productTitle ::text').get()
+        object['price'] = response.css('span.a-price-whole ::text').get()
+        object['brand'] = table[0].css('td.a-span9 span ::text').get()
+        object['memory'] = table[1].css('td.a-span9 span ::text').get()
+        object['h_interface'] = table[2].css('td.a-span9 span ::text').get()
+        object['special_features'] = table[3].css('td.a-span9 span ::text').get()
+        object['speed'] = table[4].css('td.a-span9 span ::text').get()
+        
+        yield object
+
+        
